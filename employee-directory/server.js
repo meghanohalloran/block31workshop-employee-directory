35a60bb5c13afe21138f06d6086103d0f681acb4
Meghan O'Halloran <meghaneohalloran@gmail.comuser.name meghanohallorangit pushcode>
2025-02-18 07:29:28 -0700

    final commit after remove /

--- employee-directory/server.js
+++ employee-directory/server.js
@@ -4,17 +4,17 @@ const employees = require("./employees");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Root route
+
 app.get("/", (req, res) => {
   res.send("Hello employees!");
 });
 
-// Get all employees
+
 app.get("/employees", (req, res) => {
   res.json(employees);
 });
 
-// Get employee by ID
+
 app.get("/employees/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const employee = employees.find(emp => emp.id === id);
@@ -26,7 +26,7 @@ app.get("/employees/:id", (req, res) => {
   res.json(employee);
 });
 
-// Get a random employee
+
 app.get("/employees/random", (req, res) => {
   const randomIndex = Math.floor(Math.random() * employees.length);
   res.json(employees[randomIndex]);
